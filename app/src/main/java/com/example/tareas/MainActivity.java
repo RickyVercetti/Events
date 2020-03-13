95a05e7bc6c72246ab6da01465bac0c24b14f367
Ricardo Campos Muñoz <ricky_kampos@hotmail.com>
2020-03-13 23:50:13 +0100

    Mejorada las tarjetas, añadido Toolbar y alguna cosilla más

--- app/src/main/java/com/example/tareas/MainActivity.java
+++ app/src/main/java/com/example/tareas/MainActivity.java
@@ -6,11 +6,16 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
@@ -28,15 +33,44 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        addListenerOnButtonCreate();
-        Cursor c = showOnDataBase();
+
+        recyclerView = findViewById(R.id.rv);
         emptyList = findViewById(R.id.emptyList);
+        addListenerOnButtonCreate();
+        deleteOldEvents();
+        Cursor c = getCursorFromDataBase();
 
         LinearLayoutManager manager = new LinearLayoutManager(this);
         manager.setOrientation(LinearLayoutManager.VERTICAL);
 
+        showEvents(c);
+
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.about)
+        {
+
+        }
+        else if (id == R.id.truncate)
+        {
+            //ConfirmationPrompt prompt =
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void showEvents(Cursor c) {
+
         eventList = new ArrayList<>();
-        recyclerView = findViewById(R.id.rv);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -45,6 +79,7 @@ public class MainActivity extends Activity {
         }
         while (c.moveToNext()){
             Event e = new Event(c.getString(0),c.getString(1));
+            LOG.info(e.getDate()+" son los días, el nombre: "+e.getName());
             eventList.add(e);
         }
         eventAdapter = new EventAdapter(this,eventList);
@@ -52,7 +87,18 @@ public class MainActivity extends Activity {
         c.close();
     }
 
-    protected Cursor showOnDataBase(){
+    private void deleteOldEvents() {
+
+        BBDD bd = new BBDD(getApplicationContext(),"EVENTOS", null, 1);
+        SQLiteDatabase db = bd.getWritableDatabase();
+
+        //int filesDeleted = db.delete("EVENTOS","fecha < ?",new String[] {LocalDate.now().plusDays(2000L).toString()});
+        int filesDeleted = db.delete("EVENTOS","fecha < ?",new String[] {LocalDate.now().toString()});
+        LOG.info("Filas borradas = "+filesDeleted);
+
+    }
+
+    protected Cursor getCursorFromDataBase(){
 
         BBDD bd = new BBDD(getApplicationContext(),"EVENTOS", null, 1);
         SQLiteDatabase db = bd.getWritableDatabase();
@@ -63,6 +109,7 @@ public class MainActivity extends Activity {
     protected void addListenerOnButtonCreate() {
 
         final Context context = this;
+        //FloatingActionButton buttonCreate = findViewById(R.id.addButton);
         Button buttonCreate = (Button) findViewById(R.id.addButton);
         buttonCreate.setOnClickListener(new View.OnClickListener() {
             @Override
